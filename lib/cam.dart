f87f49b544079f54a26c9bbf1448129686add317
kavin-123 <kavinkannan2024@gmail.com>
2024-12-30 03:37:39 +0530

    l

--- lib/cam.dart
+++ lib/cam.dart
@@ -14,39 +14,52 @@ class Mycam extends StatefulWidget {
 
 class _MycamState extends State<Mycam> {
   File? pickedimage_;
-  Future<void>_pickImageFromGallery() async{
-    final pickedFile = await ImagePicker().pickImage(source: ImageSource.gallery);
+  Future<void> _pickImageFromGallery() async {
+    final pickedFile =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
     setState(() {
-      pickedimage_=File(pickedFile!.path);
+      pickedimage_ = File(pickedFile!.path);
     });
   }
 
-  Future<void>_captureImageFromcamera() async{
-    final pickedFile = await ImagePicker().pickImage(source: ImageSource.camera);
+  Future<void> _captureImageFromcamera() async {
+    final pickedFile =
+        await ImagePicker().pickImage(source: ImageSource.camera);
     setState(() {
-      pickedimage_=File(pickedFile!.path);
+      pickedimage_ = File(pickedFile!.path);
     });
   }
 
-  Future<XFile>_compressimage(File image) async{
-    final compressimage= await FlutterImageCompress.compressAndGetFile(image.absolute.path, '${image.path}_compress.jpg');
+  Future<XFile> _compressimage(File image) async {
+    final compressimage = await FlutterImageCompress.compressAndGetFile(
+        image.absolute.path, '${image.path}_compress.jpg');
     return compressimage!;
   }
-
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Camera App"),),
-      body:Center(child: pickedimage_==null?Text("No Image Selected"):Image.file(pickedimage_!)) ,
+      appBar: AppBar(
+        title: Text("Camera App"),
+      ),
+      body: Center(
+          child: pickedimage_ == null
+              ? Text("No Image Selected")
+              : Image.file(pickedimage_!)),
       floatingActionButton: Row(
         mainAxisAlignment: MainAxisAlignment.center,
-
         children: [
-          FloatingActionButton(onPressed: _pickImageFromGallery,child: Icon(Icons.photo_library),),
-          SizedBox(width: 10,),
-          FloatingActionButton(onPressed: _captureImageFromcamera,child: Icon(Icons.camera_alt_outlined),),
+          FloatingActionButton(
+            onPressed: _pickImageFromGallery,
+            child: Icon(Icons.photo_library),
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          FloatingActionButton(
+            onPressed: _captureImageFromcamera,
+            child: Icon(Icons.camera_alt_outlined),
+          ),
         ],
       ),
     );
